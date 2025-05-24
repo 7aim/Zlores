ac434319efe50d417d9097cc35fb4254beed4d3e
Aim <7asim6@gmail.com>
2025-05-24 21:50:42 +0400

    -

--- zlores/post/apps.py
+++ zlores/post/apps.py
@@ -3,4 +3,4 @@ from django.apps import AppConfig
 
 class CreatepostConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'createpost'
+    name = 'post'
--- zlores/zlores/urls.py
+++ zlores/zlores/urls.py
@@ -21,5 +21,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('core.urls')),
     path('', include('accounts.urls')),
-    path('', include('createpost.urls')),
+    path('', include('post.urls')),
 ]
