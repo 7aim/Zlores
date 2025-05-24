e183d34e9baae98d9ed04a509a706175f125b02d
Aim <7asim6@gmail.com>
2025-05-24 21:25:44 +0400

    added post send future

--- zlores/createpost/views.py
+++ zlores/createpost/views.py
@@ -1,4 +1,14 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .models import Post
+from django.contrib.auth.decorators import login_required
 
+@login_required
 def createpost_request(request):
+    if request.method == 'POST':
+        title = request.POST['title']
+        content = request.POST['content']
+        post = Post.objects.create(title=title, content=content,author=request.user)
+        post.save()
+        return redirect('/')
+
     return render(request, 'createpost.html') 
